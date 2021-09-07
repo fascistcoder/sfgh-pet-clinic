35d8d9454a6b745869f027f1b60360e3dde53635
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2021-09-07 12:39:57 +0530

    List all Owners on Owner Index Page. Closes #16. List all Vets on Vet index page. Closes #15.

--- pet-clinic-web/src/main/java/com/springframework/sfghpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfghpetclinic/controllers/OwnerController.java
@@ -1,6 +1,9 @@
 package com.springframework.sfghpetclinic.controllers;
 
+import com.springframework.sfghpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String listVets() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
