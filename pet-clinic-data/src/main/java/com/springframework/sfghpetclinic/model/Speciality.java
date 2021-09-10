ec366b39f17729c93625de8d6a1f8b64939466fa
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2021-09-10 22:28:26 +0530

    Create Vet Speciality Entity, Associate To Vet. Closes #27.

---
+++ pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/model/Speciality.java
@@ -0,0 +1,18 @@
+package com.springframework.sfghpetclinic.model;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 10/09/21
+ */
+public class Speciality extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/model/Vet.java
@@ -1,9 +1,21 @@
 package com.springframework.sfghpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
  * @version 1.0
  * @since 01/09/21
  */
-public class Vet extends Person{
+public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
