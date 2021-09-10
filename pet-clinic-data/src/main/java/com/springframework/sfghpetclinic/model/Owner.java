5f544897ce153aec7bbe81f31a8d4be3cfd3cb01
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2021-09-10 22:25:26 +0530

    Create Pet Type, Pet, And Visit Entities. Closes #26.

--- pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/model/Owner.java
@@ -1,9 +1,20 @@
 package com.springframework.sfghpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
  * @version 1.0
  * @since 01/09/21
  */
 public class Owner extends Person{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/model/Visit.java
@@ -0,0 +1,38 @@
+package com.springframework.sfghpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 10/09/21
+ */
+public class Visit extends BaseEntity {
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
