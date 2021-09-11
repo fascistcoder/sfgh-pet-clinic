7ce959b2d1466bbeadde44537d76677a75e48647
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2021-09-11 14:22:32 +0530

    Added Speciality Map Service Impl . Closes #31.

---
+++ pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,11 @@
+package com.springframework.sfghpetclinic.services;
+
+import com.springframework.sfghpetclinic.model.Speciality;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 11/09/21
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,38 @@
+package com.springframework.sfghpetclinic.services.map;
+
+import com.springframework.sfghpetclinic.model.Speciality;
+import com.springframework.sfghpetclinic.services.SpecialitiesService;
+
+import java.util.Set;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 11/09/21
+ */
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findByID(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+}
