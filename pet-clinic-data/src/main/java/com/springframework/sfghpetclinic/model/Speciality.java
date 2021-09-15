0f4bb73943524ea6e492aa133c9088cab8b7fe2c
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2021-09-15 13:45:32 +0530

    Implement Vets to JPA Entities with JPA annotations . Closes #37.

--- pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/model/Speciality.java
@@ -1,11 +1,19 @@
 package com.springframework.sfghpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
  * @version 1.0
  * @since 10/09/21
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/model/Vet.java
@@ -1,5 +1,6 @@
 package com.springframework.sfghpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,8 +9,13 @@ import java.util.Set;
  * @version 1.0
  * @since 01/09/21
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+    inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
