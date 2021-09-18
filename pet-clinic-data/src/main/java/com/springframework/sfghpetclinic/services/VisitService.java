f83b1d459d558cf373f09d3c8d1cc9f5d8af5c4a
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2021-09-18 15:11:48 +0530

    Create a map service for visit. Closes #46.

---
+++ pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package com.springframework.sfghpetclinic.services;
+
+import com.springframework.sfghpetclinic.model.Visit;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 18/09/21
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/sfghpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,45 @@
+package com.springframework.sfghpetclinic.services.map;
+
+import com.springframework.sfghpetclinic.model.Visit;
+import com.springframework.sfghpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 18/09/21
+ */
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findByID(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+        || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+}
