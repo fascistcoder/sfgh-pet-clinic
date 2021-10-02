17ab1402b0e395d5cf32e640f2d68771c0a66720
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2021-10-02 23:00:16 +0530

    Implement vets JSON Endpoint. closes #69

--- pet-clinic-web/src/main/java/com/springframework/sfghpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfghpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package com.springframework.sfghpetclinic.controllers;
 
+import com.springframework.sfghpetclinic.model.Vet;
 import com.springframework.sfghpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
@@ -24,5 +29,10 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
